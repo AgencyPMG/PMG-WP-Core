f5f1ab7a327f84170f9044992f00cf956f677fcb
Christopher Davis <cdavis9999@gmail.com>
2012-10-30 13:53:42 -0500

    Inherit from the TypeBase class on PostType

--- inc/PostType.php
+++ inc/PostType.php
@@ -13,60 +13,8 @@ namespace PMG\Core;
 
 !defined('ABSPATH') && exit;
 
-class PostType
+class PostType extends TypeBase
 {
-    private $type;
-
-    private $singular;
-
-    private $plural;
-
-    private $args;
-
-    public function __construct($type, $singular, $plural, $args=array())
-    {
-        $this->type = $type;
-        $this->singular = $singular;
-        $this->plural = $plural;
-        $this->args = $args;
-        add_action('init', array($this, 'register'), 20);
-    }
-
-    /**
-     * Let users change the args for the post type directly.
-     *
-     * @since   1.0
-     * @access  public
-     * @return  void;
-     */
-    public function __set($key, $val)
-    {
-        $this->args[$key] = $val;
-    }
-
-    /**
-     * Get something from the args array.
-     *
-     * @since   1.0
-     * @access  public
-     * @return  mixed
-     */
-    public function __get($key)
-    {
-        if('type' == $key)
-            return $this->type;
-
-        return isset($this->args[$key]) ? $this->args[$key] : null;
-    }
-
-    /**
-     * Hooked into init.  Actually registers the post type.
-     *
-     * @since   1.0
-     * @access  public
-     * @uses    register_post_type
-     * @return  void
-     */
     public function register()
     {
         // let users define labels if they want.
